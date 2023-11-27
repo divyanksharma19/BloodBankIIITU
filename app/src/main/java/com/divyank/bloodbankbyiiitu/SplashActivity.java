805735fd45d3c7e72bd059ee6bdb82a3b06332f4
divyank <divyank.sharma2019@gmail.com>
2023-11-28 01:39:00 +0530

    initial

---
+++ app/src/main/java/com/divyank/bloodbankbyiiitu/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.divyank.bloodbankbyiiitu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        LottieAnimationView animationView = findViewById(R.id.animationView);
+        animationView.playAnimation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SplashScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
---
+++ app/src/main/java/com/divyank/bloodbankbyiiitu/listeners/MyOnClickListener.java
@@ -0,0 +1,5 @@
+package com.divyank.bloodbankbyiiitu.listeners;
+
+public interface MyOnClickListener {
+    void getPosition(int position);
+}
